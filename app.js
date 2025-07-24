db223ab9c59f060889a23543c43fd481a3e513cd
Cheikh Gaye Gadji <cheikhgayegadji@gmail.com>
2025-07-24 19:07:25 +0000

    feat: initial setup of user management app with HTML, CSS, and JS files.
    Co-authored-by: Genie <genie@cosine.sh>

---
+++ app.js
@@ -0,0 +1,16 @@
+// Projet JS 2025
+// Auteur : [Votre Prénom Nom]
+
+// Initialisation principale au chargement de la page
+$(document).ready(function() {
+    // Affichage initial
+    afficherUtilisateurs();
+});
+
+// Fonction qui va charger et afficher les utilisateurs
+function afficherUtilisateurs() {
+    // Ici on va récupérer les users de l'API et les afficher
+    // (À compléter plus tard)
+    $('#users-list').html('<em>Chargement des utilisateurs...</em>');
+    // Note : la suite sera développée dans les prochaines étapes
+}
